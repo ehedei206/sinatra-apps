f2e87bb736e60699ede5b13db7e10590670b3081
ehedei206 <ehedei206@msn.com>
2016-11-06 14:47:09 +1300

    Adding Profile

--- anagram_app/anagram.rb
+++ anagram_app/anagram.rb
@@ -6,14 +6,16 @@ count=word.length
 i=0
 
 while i < count do
-	fixed_letter=word(i)
-	temp_word=word
-	temp_word.delete(fixed_letter)
-	result.push(fixed_letter+temp_word)
-	result.push(fixed_letter+reverse_letters(temp_word))
+	fixed_letter=word[i]
+	temp_word=word.delete fixed_letter
+	result.push(fixed_letter << temp_word)
+	fixed_letter=word[i]
+	result.push(fixed_letter << reverse_letters(temp_word).to_s)
 	i +=1
 end
 
 return result
 
 end
+
+find_anagram('abc')
